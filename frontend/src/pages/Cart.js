36f7ca3ff068ed9c872341c9a6ffbe1f2ccab87e
kiyotone <kunwarkirtan36@gmail.com>
2024-04-13 02:19:34 +0545

    cart done

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,205 +1,245 @@
-import React, { useContext, useEffect, useState } from 'react'
-import SummaryApi from '../common'
-import Context from '../context'
-import displayINRCurrency from '../helpers/displayCurrency'
+import React, { useContext, useEffect, useState } from "react";
+import SummaryApi from "../common";
+import Context from "../context";
+import displayINRCurrency from "../helpers/displayCurrency";
 import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
-    const [data,setData] = useState([])
-    const [loading,setLoading] = useState(false)
-    const context = useContext(Context)
-    const loadingCart = new Array(4).fill(null)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const context = useContext(Context);
+  const loadingCart = new Array(4).fill(null);
 
+  const fetchData = async () => {
+    const response = await fetch(SummaryApi.addToCartProductView.url, {
+      method: SummaryApi.addToCartProductView.method,
+      credentials: "include",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
 
-    const fetchData = async() =>{
-        
-        const response = await fetch(SummaryApi.addToCartProductView.url,{
-            method : SummaryApi.addToCartProductView.method,
-            credentials : 'include',
-            headers : {
-                "content-type" : 'application/json'
-            },
-        })
-       
+    const responseData = await response.json();
 
-        const responseData = await response.json()
-
-        if(responseData.success){
-            setData(responseData.data)
-        }
-
-
+    if (responseData.success) {
+      setData(responseData.data);
     }
+  };
 
-    const handleLoading = async() =>{
-        await fetchData()
+  const handleLoading = async () => {
+    await fetchData();
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    handleLoading();
+    setLoading(false);
+  }, []);
+
+  const increaseQty = async (id, qty) => {
+    const response = await fetch(SummaryApi.updateCartProduct.url, {
+      method: SummaryApi.updateCartProduct.method,
+      credentials: "include",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        _id: id,
+        quantity: qty + 1,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (responseData.success) {
+      fetchData();
     }
+  };
 
-    useEffect(()=>{
-        setLoading(true)
-        handleLoading()
-         setLoading(false)
-    },[])
+  const decraseQty = async (id, qty) => {
+    if (qty >= 2) {
+      const response = await fetch(SummaryApi.updateCartProduct.url, {
+        method: SummaryApi.updateCartProduct.method,
+        credentials: "include",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          _id: id,
+          quantity: qty - 1,
+        }),
+      });
 
+      const responseData = await response.json();
 
-    const increaseQty = async(id,qty) =>{
-        const response = await fetch(SummaryApi.updateCartProduct.url,{
-            method : SummaryApi.updateCartProduct.method,
-            credentials : 'include',
-            headers : {
-                "content-type" : 'application/json'
-            },
-            body : JSON.stringify(
-                {   
-                    _id : id,
-                    quantity : qty + 1
-                }
-            )
-        })
-
-        const responseData = await response.json()
-
-
-        if(responseData.success){
-            fetchData()
-        }
+      if (responseData.success) {
+        fetchData();
+      }
     }
+  };
 
+  const deleteCartProduct = async (id) => {
+    const response = await fetch(SummaryApi.deleteCartProduct.url, {
+      method: SummaryApi.deleteCartProduct.method,
+      credentials: "include",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        _id: id,
+      }),
+    });
 
-    const decraseQty = async(id,qty) =>{
-       if(qty >= 2){
-            const response = await fetch(SummaryApi.updateCartProduct.url,{
-                method : SummaryApi.updateCartProduct.method,
-                credentials : 'include',
-                headers : {
-                    "content-type" : 'application/json'
-                },
-                body : JSON.stringify(
-                    {   
-                        _id : id,
-                        quantity : qty - 1
-                    }
-                )
-            })
+    const responseData = await response.json();
 
-            const responseData = await response.json()
-
-
-            if(responseData.success){
-                fetchData()
-            }
-        }
+    if (responseData.success) {
+      fetchData();
+      context.fetchUserAddToCart();
     }
+  };
 
-    const deleteCartProduct = async(id)=>{
-        const response = await fetch(SummaryApi.deleteCartProduct.url,{
-            method : SummaryApi.deleteCartProduct.method,
-            credentials : 'include',
-            headers : {
-                "content-type" : 'application/json'
-            },
-            body : JSON.stringify(
-                {   
-                    _id : id,
-                }
-            )
-        })
-
-        const responseData = await response.json()
-
-        if(responseData.success){
-            fetchData()
-            context.fetchUserAddToCart()
-        }
-    }
-
-    const totalQty = data.reduce((previousValue,currentValue)=> previousValue + currentValue.quantity,0)
-    const totalPrice = data.reduce((preve,curr)=> preve + (curr.quantity * curr?.productId?.sellingPrice) ,0)
+  const totalQty = data.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.quantity,
+    0
+  );
+  const totalPrice = data.reduce(
+    (preve, curr) => preve + curr.quantity * curr?.productId?.sellingPrice,
+    0
+  );
   return (
-    <div className='container mx-auto'>
-        
-        <div className='text-center text-lg my-3'>
-            {
-                data.length === 0 && !loading && (
-                    <p className='bg-white py-5'>No Data</p>
-                )
-            }
+    <div className="container mx-auto">
+      <div className="text-center text-lg my-3">
+        {data.length === 0 && !loading && (
+          <p className="bg-white py-5">No Data</p>
+        )}
+      </div>
+
+      <div className="flex flex-col mx-[8rem] lg:flex-row lg:justify-between p-4">
+        <div className="w-full max-w-[50rem] ">
+          <div className="border-x border-t h-[3rem] p-2 text-xl border-slate-600">
+            SHOPPING CART
+          </div>
+
+          <div className="border pb-10 gap-y-10 pt-10 flex flex-col items-center border-slate-600">
+            {loading
+              ? loadingCart?.map((el, index) => {
+                  return (
+                    <div
+                      key={el + "Add To Cart Loading" + index}
+                      className="w-full bg-slate-200 h-32 my-2 border border-slate-300 animate-pulse rounded"
+                    ></div>
+                  );
+                })
+              : data.map((product, index) => {
+                  return (
+                    <div
+                      key={product?._id + "Add To Cart Loading"}
+                      className="w-full  px-6  rounded flex items-center gap-x-32"
+                    >
+                      <div className="w-32 h-32 bg-slate-200">
+                        <img
+                          src={product?.productId?.productImage[0]}
+                          className="w-full h-full object-scale-down mix-blend-multiply"
+                        />
+                      </div>
+
+                      <div className="w-32 h-32">
+                        <h2 className="text-lg lg:text-xl text-ellipsis line-clamp-1">
+                          {product?.productId?.productName}
+                        </h2>
+                        <p className="capitalize text-slate-500">
+                          {product?.productId.category}
+                        </p>
+                        <div className="flex items-center justify-between">
+                          <p className="text-red-600 font-medium text-lg">
+                            {displayINRCurrency(
+                              product?.productId?.sellingPrice
+                            )}
+                          </p>
+                        </div>
+                        <div className="flex items-center gap-3 mt-1 w-28 shadow-md p-2 pl-4">
+                          <button
+                            className="border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded=2xl"
+                            onClick={() =>
+                              decraseQty(product?._id, product?.quantity)
+                            }
+                          >
+                            -
+                          </button>
+                          <span>{product?.quantity}</span>
+                          <button
+                            className="border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded=2xl"
+                            onClick={() =>
+                              increaseQty(product?._id, product?.quantity)
+                            }
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="pl-28 w-52 h-32 py-2 flex items-center flex-col ">
+                        {/**delete product */}
+                        <div
+                          className=" text-red-600 rounded-md p-2 border border-transparent hover:border-red-600 cursor-pointer"
+                          onClick={() => deleteCartProduct(product?._id)}
+                        >
+                          <MdDelete />
+                        </div>
+                        <p className="text-slate-600 font-semibold text-lg">
+                          {displayINRCurrency(
+                            product?.productId?.sellingPrice * product?.quantity
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+          </div>
         </div>
 
-        <div className='flex flex-col lg:flex-row gap-10 lg:justify-between p-4'>   
-                {/***view product */}
-                <div className='w-full max-w-3xl'>
-                    {
-                        loading ? (
-                            loadingCart?.map((el,index) => {
-                                return(
-                                    <div key={el+"Add To Cart Loading"+index} className='w-full bg-slate-200 h-32 my-2 border border-slate-300 animate-pulse rounded'>
-                                    </div>
-                                )
-                            })
-                             
-                        ) : (
-                          data.map((product,index)=>{
-                           return(
-                            <div key={product?._id+"Add To Cart Loading"} className='w-full bg-white h-32 my-2 border border-slate-300  rounded grid grid-cols-[128px,1fr]'>
-                                <div className='w-32 h-32 bg-slate-200'>
-                                    <img src={product?.productId?.productImage[0]} className='w-full h-full object-scale-down mix-blend-multiply' />
-                                </div>
-                                <div className='px-4 py-2 relative'>
-                                    {/**delete product */}
-                                    <div className='absolute right-0 text-red-600 rounded-full p-2 hover:bg-red-600 hover:text-white cursor-pointer' onClick={()=>deleteCartProduct(product?._id)}>
-                                        <MdDelete/>
-                                    </div>
-
-                                    <h2 className='text-lg lg:text-xl text-ellipsis line-clamp-1'>{product?.productId?.productName}</h2>
-                                    <p className='capitalize text-slate-500'>{product?.productId.category}</p>
-                                    <div className='flex items-center justify-between'>
-                                            <p className='text-red-600 font-medium text-lg'>{displayINRCurrency(product?.productId?.sellingPrice)}</p>
-                                            <p className='text-slate-600 font-semibold text-lg'>{displayINRCurrency(product?.productId?.sellingPrice  * product?.quantity)}</p>
-                                    </div>
-                                    <div className='flex items-center gap-3 mt-1'>
-                                        <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded ' onClick={()=>decraseQty(product?._id,product?.quantity)}>-</button>
-                                        <span>{product?.quantity}</span>
-                                        <button className='border border-red-600 text-red-600 hover:bg-red-600 hover:text-white w-6 h-6 flex justify-center items-center rounded ' onClick={()=>increaseQty(product?._id,product?.quantity)}>+</button>
-                                    </div>
-                                </div>    
-                            </div>
-                           )
-                          })
-                        )
-                    }
+        {/***summary  */}
+        <div className="mt-5 lg:mt-0 text-base w-full max-w-sm">
+          {loading ? (
+            <div className="h-36 border border-slate-300 text-black animate-pulse"></div>
+          ) : (
+            <div className=" h-36">
+              <h2 className="border-x border-t border-slate-600  px-4 py-1 h-[3rem]">
+                Summary
+              </h2>
+              <div className="border p-7 border-slate-600 px-4 gap-5">
+                <div className="flex items-center pb-2 justify-between  text-slate-600">
+                  <p>{totalQty} items</p>
+                  <p>{displayINRCurrency(totalPrice)}</p>
                 </div>
 
-
-                {/***summary  */}
-                <div className='mt-5 lg:mt-0 w-full max-w-sm'>
-                        {
-                            loading ? (
-                            <div className='h-36 bg-slate-200 border border-slate-300 animate-pulse'>
-                                
-                            </div>
-                            ) : (
-                                <div className='h-36 bg-white'>
-                                    <h2 className='text-white bg-red-600 px-4 py-1'>Summary</h2>
-                                    <div className='flex items-center justify-between px-4 gap-2 font-medium text-lg text-slate-600'>
-                                        <p>Quantity</p>
-                                        <p>{totalQty}</p>
-                                    </div>
-
-                                    <div className='flex items-center justify-between px-4 gap-2 font-medium text-lg text-slate-600'>
-                                        <p>Total Price</p>
-                                        <p>{displayINRCurrency(totalPrice)}</p>    
-                                    </div>
-
-                                    <button className='bg-blue-600 p-2 text-white w-full mt-2'>Payment</button>
-
-                                </div>
-                            )
-                        }
+                <div className="flex items-center justify-between  text-slate-600">
+                  <p>Shipping</p>
+                  <p>NPR 2000</p>
                 </div>
+              </div>
+              <div className="border-x border-b p-7 border-slate-600 px-4 gap-5">
+                <div className="flex items-center pb-2 justify-between  text-slate-600">
+                  <p>Taxes</p>
+                  <p>NRS 123103</p>
+                </div>
+
+                <div className="flex items-center justify-between  text-slate-600">
+                  <p>Grand Total</p>
+                  <p>NPR 2000</p>
+                </div>
+              </div>
+              <div className="border-x border-b p-7 border-slate-600">
+                <button className="bg-blue-600 p-2 text-white w-full mt-2">
+                  Payment
+                </button>
+              </div>
+            </div>
+          )}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
