a7fc2bc9b4398aef6c8a58933c1645e2c3d25819
pr-a-sh-ant <prashantaryal07@gmail.com>
2024-04-14 12:17:56 +0545

    HomePage changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
-        "mini": "1100px",
-        "mobile": "640px",
+        mini: "1100px",
+        mobile: "640px",
+      },
+      backgroundImage: {
+        "back-1": "url('../public/bakgrnd.png')",
+        "back-2": "url('../public/bakgrnd2.jpg')",
+        "back-3": "url('../public/bakgrnd3.jpg')",
+        "back-4": "url('../public/bakgrnd4.jpg')",
       },
     },
   },
   plugins: [],
-}
-
+};
